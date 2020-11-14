78af9e01c5ba3ee41aaaaf61ed055b41e6b4c233
alim <saban29@mail.ru>
2020-11-14 22:25:57 +0300

    поправки в auth

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -18,7 +18,7 @@ const Auth = () => {
         firebase.initializeApp(firebaseConfig);
         
         var uiConfig = {
-            signInSuccessUrl: 'index.html',
+            signInSuccessUrl: '/',
             signInOptions: [
                 // Leave the lines as is for the providers you want to offer your users.
                 firebase.auth.GoogleAuthProvider.PROVIDER_ID
